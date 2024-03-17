774aac0cd6a86c24daa94752b4245f81590e3451
Ajay v <vajay4834@gmail.com>
2024-03-17 16:53:07 +0530

    apis added

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var contactRouter = require('./routes/contactRoute');
-
+var contactRouter = require("./routes/contactRoute");
+const connectDb = require("./config/dbConnection");
+require("dotenv").config();
 var app = express();
-
+connectDb();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 // app.engine('hbs',hbs({extname}))
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api/contact', contactRouter);
+app.use("/api/contact", contactRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  const status = err.status ? err.status : 500;
+  res.status(status);
+  res.render("error");
+  // res.status(status).json({
+  //   error:res.locals.error,
+  // })
 });
 
 module.exports = app;
--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,18 +1,63 @@
-const getContacts = (req, res) => {
-  res.status(200).json({ message: "get all contacts" });
+const contact = require("../models/contactModel");
+const getContacts = async (req, res) => {
+  const contacts = await contact.find();
+  res.status(200).json({ message: "get all contacts", data: contacts });
 };
-const createContact = (req, res) => {
-  res.status(201).json({ message: "create contacts" });
+const createContact = async (req, res) => {
+  const { name, email, phone } = req.body;
+
+  if (!name || !email || !phone) {
+    res.status(400).json({
+      message: "allfields are mandratory",
+    });
+  }
+  const quey = await contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(201).json({
+    message: "create contacts",
+    data: quey,
+  });
 };
 
-const getContact = (req, res) => {
-  res.status(200).json({ message: `get contact of ${req.params.id}` });
+const getContact = async (req, res) => {
+  try {
+    const query = await contact.findById(req.params.id);
+    if (!query) {
+      return res.status(400).json({ message: "Cant Find" });
+    }
+    return res
+      .status(200)
+      .json({ message: `get contact of ${req.params.id}`, data: query });
+  } catch (err) {
+    res.status(400).json({ message: "Cant Find" });
+    console.log(err);
+  }
 };
-const updateContact = (req, res) => {
-  res.status(200).json({ message: `update contact of ${req.params.id}` });
+const updateContact = async (req, res) => {
+  const query = await contact.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res
+    .status(200)
+    .json({ message: `update contact of ${req.params.id}`, data: query });
 };
-const deleteContact = (req, res) => {
-  res.status(200).json({ message: `delete contact of ${req.params.id}` });
+const deleteContact = async (req, res) => {
+  try {
+    const query = await contact.findById(req.params.id);
+    if (!query) {
+      return res.status(400).json({ message: "Cant Find" });
+    }
+    await query.deleteOne({_id:req.params.id});
+    return res
+      .status(200)
+      .json({ message: `delete contact of ${req.params.id}`, data: query });
+  } catch (err) {
+    res.status(400).json({ message: "Cant Find" });
+    console.log(err);
+  }
 };
 
 module.exports = {
